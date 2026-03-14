23df1877f853161dfb51caa7bb00be7b03b03af8
Melroy Barreto <melroybarreto61@gmail.com>
2026-03-15 00:29:02 +0530

    rename controllers, add post route for book notes

---
+++ server/controllers/postUserNotesController.js
@@ -0,0 +1,5 @@
+import db from "../db/index.js";
+
+export const postUserNotes = async (req, res) => {
+  const userID = "";
+};
--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -3,13 +3,17 @@ import {
   getBooks,
   searchBooks,
   getBookDetails,
-} from "../controllers/bookController.js";
+} from "../controllers/fetchBookController.js";
+import { postUserNotes } from "../controllers/postUserNotesController.js";
 
 const router = express.Router();
 
 //GET /api/books/search - Get all books (search for book in DB or external API)
 router.get("/search", searchBooks);
-//GET /api/books/user/:id - Get all book notes for a user
+//GET /api/books/book/:bookId - Get book details using the book ID
 router.get("/book/:bookId", getBookDetails);
+
+//POST /api/books/notes
+router.post("/notes", postUserNotes);
 
 export default router;
